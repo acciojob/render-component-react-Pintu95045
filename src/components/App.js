ad09bd20070a53e074dcfccf267c804705bd9e53
Pintu Giri <mrpintugiri@gmail.com>
2024-10-08 02:04:13 +0530

    h tag added

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="root">
+      <h1>This is the React Project</h1>
     <p>I am learning React. My life is getting better.</p>
     </div>
   )
