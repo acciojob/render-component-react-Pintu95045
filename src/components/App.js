7588f45089bef722899a840b1fed8af59ef4c084
Pintu Kumar Giri <131274412+Pintu95045@users.noreply.github.com>
2024-10-12 12:40:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,12 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-        <p>
-        I am learning React. My life is getting better.
-        </p>
+      <p>
+        I am learning React. My life is getting better. 
+      </p>
     </div>
-  );
+  )
 }
 
 
-export default App; 
+export default App;
