d1ca75cbe574805913af1994fe4cd70795e3f655
Pintu Giri <mrpintugiri@gmail.com>
2024-10-12 17:36:53 +0530

    solution updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
-  return (
+return (
+    <div id="main"></div>
     <div id="main">
       <p>
         I am learning React. My life is getting better. 
       </p>
     </div>
-  )
+)
 }
 
 
